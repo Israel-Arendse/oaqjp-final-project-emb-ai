38cfd6b9f37993527a3674db03925de4e64d342c
Codemason43 <145558386+Israel-Arendse@users.noreply.github.com>
2024-09-24 12:43:01 +0400

    Update emotion_detection.py (Add base code and import requests)
    In this commit,  I implemented the import statement for the requests library in line 2
    
    From lines 5 to 15, the code for the 'emotion_detector' function is implemented with a header, URL, JSON_input, and response:
    
      • Line 7 contains the URL for the Watson Natural Language Library's emotion detector service.
    
      • Line 9 sets the headers for the API post request.
    
      • Line 11 initializes the dictionary 'textobj' as "text", which is used for the actual text analysis.
    
      • Line 13 sends the POST request for the emotion detector API.
    
      • Line 15 retrieves the response text from the API, providing exact information about the emotion in the text.

---
+++ emotion_detection.py
@@ -0,0 +1,16 @@
+# import the request library to handle HTTP requirements
+import requests 
+
+# Define the function 'emotion_detector' to take the string input (text_to_analyse)
+def emotion_detector(text_to_analyze):
+    #  Define the URL for the emotion detection service
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1./NlpService/EmotionPredict'
+    # Set the headers required for the API request.
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    # Create the dictionary 'textobj' with the text to be analyzed under the variable 'text'
+    textobj = { "raw_document": { "text": text_to_analyze} }
+    # Send a POST request to the API with the text and headers
+    response = requests.post.(url, headers=header, json = textobj)
+    # Return the response text from the API
+    return response.text
+
