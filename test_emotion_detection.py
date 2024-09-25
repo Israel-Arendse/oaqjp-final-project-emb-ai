bc2e00fe3c95c0558efcc86903208e8b923341f3
Israel-Arendse <israelarendefortnite@gmail.com>
2024-09-25 12:05:06 -0400

    Rewrote 'emotion' values to 'dominant_emotion' in test_emotion_detection.py

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -11,27 +11,27 @@ class test_emotion_detection(unittest.TestCase):
       # Test case for dominant emotion: joy
       # The dominant emotion is joy when "I am glad this happend' is sent as input.
       test_1 = emotion_detector('I am glad this happend')
-      self.assertEqual(test_1['emotion'], 'joy')
+      self.assertEqual(test_1['dominant_emotion'], 'joy')
       
       # Test case for dominant emotion: anger
       # The dominant emotion is anger when 'I am really mad about this' is sent as input.
       test_2 = emotion_detector('I am really mad about this')
-      self.assertEqual(test_2['emotion'], 'anger')
+      self.assertEqual(test_2['dominant_emotion'], 'anger')
 
       # Test case for dominant emotion: disgust
       # The dominant emotion is disgust when 'I feel disgusted just about this' is sent as input.
       test_3 = emotion_detector('I feel disgusted just hearing about this')
-      self.assertEqual(test_3['emotion'], 'disgust')
+      self.assertEqual(test_3['dominant_emotion'], 'disgust')
 
       # Test case for dominant emotion: sadness
       # The dominant emotion is sadness when 'I feel so sad about this' is sent as input.
       test_4 = emotion_detector('I am so sad about this')
-      self.assertEqual(test_4['emotion'], 'sadness')
+      self.assertEqual(test_4['dominant_emotion'], 'sadness')
 
       # Test case for dominant emotion: fear
       # The dominant emotion is fear when 'I am really afraid that this will happen' is sent as input.
       test_5 = emotion_detector('I am really afraid this will happend')
-      self.assertEqual(test_5['emotion'], 'fear')
+      self.assertEqual(test_5['dominant_emotion'], 'fear')
 
 
 # Run all the test casess in the module upon script execution
